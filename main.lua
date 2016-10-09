6abc4111289970432d8bb58124403bc6201c5ae6
Программирование <>
2016-10-09 13:29:32 +0200

    Подщёт смертей и цвет Имён

--- main.lua
+++ main.lua
@@ -17,9 +17,12 @@ local game = display.newGroup();
 local face = display.newGroup();
 local misses = 0;
 local misstxt = display.newText(face,"Hellow world",W/2,30,nil,20)
+local hits = 0;
+local hitstxt = display.newText(face,"hits",W/2,50,nil,20)
 
 function refresh()
 	misstxt.text = misses;
+	hitstxt.text = hits;
 end
 
 function getWord()
@@ -36,10 +39,17 @@ function addEnemy()
 	unit:setDir(100,0);
 	unit:setAct("go");
 	mc.y = math.random(1,#words)*H/#words - H/10;
------------------------Добовление текста на экран---------
+-----------------------Добовление текста на экран и гоблинов---------
 	local word = getWord();
 	mc.word = word;
 	local dtxt = display.newText(mc,word,0,0,nil,30);
+	function mc:doDeath()
+		mc.body:setAct("death");
+		dtxt:setTextColor(1,0,0);
+		transition.to(dtxt,{time = 2000,y = dtxt.y-100,alpha = 0});
+	end
+	dtxt.y = -30;
+	mc.dtxt = dtxt;
 --------------------------------------------------
 	table.insert(enemies,mc)
 end;
@@ -66,7 +76,8 @@ Runtime:addEventListener("enterFrame",function()
 			if(mc.x > W) then
 				misses = misses + 1;
 				refresh();
-				mc.body:setAct("death");
+				--mc.body:setAct("death");
+				mc:doDeath();
 				mc.dead = true;
 			end;
 		end;
@@ -79,14 +90,19 @@ Runtime:addEventListener('key', function (event)
 	if(event.phase=="down")then
 		table.insert(keys,event.keyName);
 		print(event.phase, event.keyName,table.concat(keys,""));
-		local word = table.concat(keys,'')
-		for i=#enemies,1,-1 do
-			local mc = enemies[i];
-			if (mc.word==word)then
-				mc.body:setAct("death");
-				table.remove(enemies,i);
-				keys = {};
-				break;
+		for j=#keys,1,-1 do
+			local word = table.concat(keys,"",j,#keys)
+			for i=#enemies,1,-1 do
+				local mc = enemies[i];
+				if (mc.word==word)then
+					--mc.body:setAct("death");
+					mc:doDeath();
+					hits = hits + 1;
+					refresh();
+					table.remove(enemies,i);
+					keys = {};
+					break;
+				end;
 			end;
 		end;
 	end;
